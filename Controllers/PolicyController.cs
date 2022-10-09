ea65abed17fb397806f14b9fcb9539cc0f58feb4
shylo229 <58787144+shylo229@users.noreply.github.com>
2022-10-09 21:14:54 +0200

    Update PolicyController.cs

--- Controllers/PolicyController.cs
+++ Controllers/PolicyController.cs
@@ -17,64 +17,120 @@ namespace policystore.Controllers
 
         // GET: api/<PolicyController>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public ActionResult<List<Policy>> Get()
         {
-            return Ok(_policyRepository.AsQueryable());
+            try
+            {
+                return Ok(_policyRepository.AsQueryable());
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error. Please try again later.");
+            }
+            
         }
 
         // GET api/<PolicyController>/5
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<Policy> Get(string id)
         {
-            var policy = _policyRepository.FindById(id);
-
-            if (policy == null)
+            try
             {
-                return NotFound($"Policy with Id : {id} not found");
+                var policy = _policyRepository.FindById(id);
+
+                if (policy == null)
+                {
+                    return NotFound($"Policy with Id : {id} not found");
+                }
+                return policy;
             }
-            return policy;
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error. Please try again later.");
+            }
+
         }
 
         // POST api/<PolicyController>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public ActionResult<Policy> Post([FromBody] Policy policy)
         {
-            _policyRepository.InsertOne(policy);
+            try
+            {
+                _policyRepository.InsertOne(policy);
 
-            return CreatedAtAction(nameof(Get), new { id = policy.Id }, policy);
+                return CreatedAtAction(nameof(Get), new { id = policy.Id }, policy);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error. Please try again later.");
+            }
+
         }
 
         // PUT api/<PolicyController>/5
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<Policy> Put(string id, [FromBody] Policy policy)
         {
-
-            var existingPolicy = _policyRepository.FindById(id);
-
-            if (existingPolicy == null)
+            try
             {
-                return NotFound($"Policy with Id : {id} not found");
+                var existingPolicy = _policyRepository.FindById(id);
+
+                if (existingPolicy == null)
+                {
+                    return NotFound($"Policy with Id : {id} not found");
+                }
+
+                _policyRepository.ReplaceOne(existingPolicy.Id, policy);
+
+                return Ok(policy);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error. Please try again later.");
             }
 
-            _policyRepository.ReplaceOne(existingPolicy.Id, policy);
-
-            return NoContent();
         }
 
         // DELETE api/<PolicyController>/5
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<Policy> Delete(string id)
         {
-            var existingPolicy = _policyRepository.FindById(id);
-
-            if (existingPolicy == null)
+            try
             {
-                return NotFound($"Policy with Id : {id} not found");
+                var existingPolicy = _policyRepository.FindById(id);
+
+                if (existingPolicy == null)
+                {
+                    return NotFound($"Policy with Id : {id} not found");
+                }
+
+                _policyRepository.DeleteById(existingPolicy.Id.ToString());
+
+                return Ok($"Policy with Id : {id} deleted");
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error. Please try again later.");
             }
 
-            _policyRepository.DeleteById(existingPolicy.Id.ToString());
-
-            return Ok($"Policy with Id : {id} deleted");
         }
     }
 }
